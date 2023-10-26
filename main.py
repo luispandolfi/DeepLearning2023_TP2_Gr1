8dcfcc245768c32bcf149a2ac4a9fe370bb8eec2
Luis Pandolfi <lpandolfi@infragistics.com>
2023-10-26 09:59:03 -0300

    Agrego clase Recommender (singleton) como implementador de los servicios Se mueve el main de los servicios fastapi para evitar errores de compilacion

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
-from recommender import Recommender
+from services.recommender import Recommender
 
 app = FastAPI()
 
 
 @app.get("/peliculas/recomendar/{k_peliculas}/usuario/{id_usuario}")
 def recomendar_peliculas(id_usuario: int, k_peliculas: int):
-    return Recommender.recomendar_peliculas(id_usuario, k_peliculas)
+    return Recommender.get_instance().recomendar_peliculas(id_usuario, k_peliculas)
 
 
 @app.get("/peliculas/similares/{k_peliculas}/pelicula/{id_pelicula}")
 def peliculas_similares(id_pelicula: int, k_peliculas: int):
-    return Recommender.peliculas_similares(id_pelicula, k_peliculas)
+    return Recommender.get_instance().peliculas_similares(id_pelicula, k_peliculas)
--- services/recommender.py
+++ services/recommender.py
@@ -1,9 +1,32 @@
+from data.database_manager import DatabaseManager
+
 class Recommender:
 
+    singleton_instance = None
+
     @classmethod
-    def recomendar_peliculas(cls, id_usuario: int, k_peliculas: int):
-        return {"id_usuario": id_usuario, "K": k_peliculas}
+    def get_instance(cls):
+        if not cls.singleton_instance:
+            cls.singleton_instance = Recommender()
+        return cls.singleton_instance
     
-    @classmethod
-    def peliculas_similares(cls, id_pelicula: int, k_peliculas: int):
+
+    def __init__(self):
+        self.databaseManager = DatabaseManager("postgresql://admin:Password01@127.0.0.1/sisrec")
+    
+    
+    def recomendar_peliculas(self, id_usuario: int, k_peliculas: int):
+        recomendadas = self.databaseManager.get_top_k_peliculas_rankeadas(k_peliculas, id_usuario)
+        resultado = []
+        for pelicula, puntaje in recomendadas:
+            recomendada = {}
+            recomendada['nombre'] = pelicula.nombre
+            recomendada['fecha_estreno'] = pelicula.fecha_estreno
+            recomendada['url'] = pelicula.url
+            recomendada['puntaje'] = puntaje
+            resultado.append(recomendada)
+        return resultado
+    
+
+    def peliculas_similares(self, id_pelicula: int, k_peliculas: int):
         return {"id_pelicula": id_pelicula, "K": k_peliculas}
